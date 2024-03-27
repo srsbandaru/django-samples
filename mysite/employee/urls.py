d2864cc5b2b3bf1da86aec20c1131fe7e4d078bb
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2024-03-27 17:27:55 +1100

    Department Model
    CRUD Operations for Department Model from User Interface

--- mysite/employee/urls.py
+++ mysite/employee/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "employee"
 
 urlpatterns = [
-    path('', views.DepartmentList.as_view(), name = 'all')
+    path('', views.DepartmentList.as_view(), name = 'all'),
+    path('department/create/', views.CreateDepartment.as_view(), name = 'create_department'),
+    path('department/edit/<int:pk>', views.EditDepartment.as_view(), name = 'edit_department'),
+    path('department/delete/<int:pk>', views.DeleteDepartment.as_view(), name = 'delete_department')
 ]
--- mysite/employee/views.py
+++ mysite/employee/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Department
+from employee.forms import DepartmentForm
 from django.views import View
+from django.contrib import messages
 
 # Create your views here.
 
@@ -15,5 +17,71 @@ class DepartmentList(View):
         }
         return render(request, self.template, context)
     
+# Create Department View
+class CreateDepartment(View):
+    template = 'employee/department_form.html'
+    success_url = 'employee:all'
 
+    def get(self, request):
+        form = DepartmentForm()
+        context = {
+            'form':form
+        }
+        return render(request, self.template, context)
     
+    def post(self, request):
+        form = DepartmentForm(request.POST)
+        if not form.is_valid():
+            context = {
+                'form':form
+            }
+            return render(request, self.template, context)
+        form.save()
+        messages.success(request, "Department was successfully created.")
+        return redirect(self.success_url)
+    
+# Edit Department View 
+class EditDepartment(View):
+    model = Department
+    template = "employee/department_form.html"
+    success_url = "employee:all"
+    
+    def get(self, request, pk):
+        department = get_object_or_404(self.model, id=pk)
+        form = DepartmentForm(instance=department)
+        context = {
+            'form':form
+        }
+        return render(request, self.template, context)
+    
+    def post(self, request, pk):
+        department = get_object_or_404(self.model, id=pk)
+        form = DepartmentForm(request.POST,instance=department)
+        if not form.is_valid():
+            context = {
+                'form':form
+            }
+            messages.error(request, "Please enter all the required details.")
+            return render(request, self.template, context)
+        form.save()
+        messages.success(request, "Department was successfully updated.")
+        return redirect(self.success_url)
+    
+# Delete Department View
+class DeleteDepartment(View):
+    model = Department
+    template = "employee/department_confirm_delete.html"
+    success_url = "employee:all"
+
+    def get(self, request, pk):
+        department = get_object_or_404(self.model, id=pk)
+        context = {
+            "department":department
+        }
+        return render(request, self.template, context)
+    
+    def post(self, request, pk):
+        department = get_object_or_404(self.model, id=pk)
+        department.delete()
+        messages.info(request, "Department was successfully deleted.")
+        return redirect(self.success_url)
